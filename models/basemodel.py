995f4bbabef9123045e51adeb9a9ee2826ce8245
thatmehdude <104757542+thatmehdude@users.noreply.github.com>
2023-05-11 10:22:20 +0100

    added descriptions

--- models/basemodel.py
+++ models/basemodel.py
@@ -16,15 +16,20 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
     def save(self):
+        """update updated_at with the current time"""
         self.updated_at = datetime.datetime.now()
 
     def __str__(self):
+        """Returns a string represnetation of the instance"""
         return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
     
     def to_dict(self):
+        """
+        Returns a dictionary with keys and values of the instance
+        created_at and updated_at are converted to string object in ISO format 
+        """
         new_dict = self.__dict__.copy()
         new_dict["__class__"] = type(self).__name__
         new_dict["created_at"] = self.created_at.isoformat()
         new_dict["updated_at"] = self.updated_at.isoformat()
         return new_dict
-        
