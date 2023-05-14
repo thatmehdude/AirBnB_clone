e00fd487809f37e67facee712cf804d9a2e2ccf2
thatmehdude <104757542+thatmehdude@users.noreply.github.com>
2023-05-14 21:43:03 +0100

    corrected some errors

--- models/base_model.py
+++ models/base_model.py
@@ -11,9 +11,9 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """initialiser"""
-        self.id = str(uuid.uuif4())
-        self.created_at = datetime.datetime.now()
-        self.updated_at = datetime.datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """update updated_at with the current time"""
